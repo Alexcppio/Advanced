e1e050858673339a86a68cf3b64eb17e6d7bd4cd
Alexcppio <test21112019test@gmail.com>
2021-12-03 15:59:40 +0500

    blazor forms add

--- Models/Person.cs
+++ Models/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Advanced.Models
 {
@@ -7,9 +8,21 @@ namespace Advanced.Models
     {
 
         public long PersonId { get; set; }
+
+        [Required(ErrorMessage = "A firstname is required")]
+        [MinLength(3, ErrorMessage = "Firstnames must be 3 or more characters")]
         public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "A surname is required")]
+        [MinLength(3, ErrorMessage = "Surnames must be 3 or more characters")]
         public string Surname { get; set; }
+
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A department must be selected")]
         public long DepartmentId { get; set; }
+
+        [Required]
+        [Range(1, long.MaxValue, ErrorMessage = "A location must be selected")]
         public long LocationId { get; set; }
 
         public Department Department { get; set; }
